b7a1a91d41b87c80dd8ddf61a46be0ca8d2f30d7
Lucas Nascimento <purp0s3@icloud.com>
2024-02-29 23:02:19 -0300

    all done?... en-US

--- v2/assets/js/main.js
+++ v2/assets/js/main.js
@@ -56,4 +56,4 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.skills__data, .work__img, .education__img',{interval: 200}); 
